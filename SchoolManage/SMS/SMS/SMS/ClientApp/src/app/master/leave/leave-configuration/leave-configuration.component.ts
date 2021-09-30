deeee468c122914716ec8d8e60ab97014370db32
Arun Prasanth M <arunmrav1034@gmail.com>
2021-10-01 00:56:48 +0530

    leave module fixes

--- SchoolManage/SMS/SMS/SMS/ClientApp/src/app/master/leave/leave-configuration/leave-configuration.component.ts
+++ SchoolManage/SMS/SMS/SMS/ClientApp/src/app/master/leave/leave-configuration/leave-configuration.component.ts
@@ -10,6 +10,8 @@ export class LeaveConfigurationComponent implements OnInit {
 
   leaveForm : FormGroup;
   staffLeave: FormGroup;
+  enabletrashForStudentLeave : any = true;
+  enabletrashForStaffLeave : any = true;
 
   @Output() formDetails=new EventEmitter();
 
@@ -18,45 +20,99 @@ export class LeaveConfigurationComponent implements OnInit {
   };
 
   get staffLeavearr(): FormArray {
-    return this.staffLeave.get('staffLeave') as FormArray;
+    return this.leaveForm.get('staffLeavearr') as FormArray;
   };
 
   constructor(private fb : FormBuilder) {
     this.leaveForm = this.fb.group({
       studentLeave: this.fb.array([this.buildStudentLeave()]),
      // staffLeave: this.fb.array([this.buildStaffLeave]),
-      staffLeave : this.fb.group({
+      // staffLeave : this.fb.group({
         staffLeavearr : this.fb.array([this.buildStaffLeave])
-      })
+      // })
     });
 
-    this.leaveForm.valueChanges.subscribe(()=>{
-      this.formDetails.emit({ value: this.leaveForm.value,
-               valid: this.leaveForm.valid });
-    });
+    // this.leaveForm.valueChanges.subscribe(()=>{
+    //   this.formDetails.emit({ value: this.leaveForm.value,
+    //            valid: this.leaveForm.valid });
+    // });
+
+    // this.staffLeave.valueChanges.subscribe(()=>{
+    //   this.formDetails.emit({ value: this.staffLeave.value,
+    //            valid: this.staffLeave.valid });
+    // });
    }
 
   ngOnInit(): void {
   }
+
   addStudentleave(): void {
     this.studentLeave.push(this.buildStudentLeave());
   }
 
   addStaffleave(): void {
-    this.studentLeave.push(this.buildStaffLeave());
+    this.staffLeavearr.push(this.buildStaffLeave());
   }
 
   buildStudentLeave(): FormGroup {
     return this.fb.group({
-      studentLeave: '',
+      studentLeave: ['', Validators.required]
     });
     
   }
   buildStaffLeave(): FormGroup {
     return this.fb.group({
-      staffLeave: '',
+      staffLeave: ['', Validators.required]
     });
     
+  }
+
+  deleteForStudentLeave(index: number)
+  {
+    //console.log(this.studentLeave.at(index).value.inventoryDefectId);
+    // this.inventoryDefectService.deleteInventoryDefects(this.defect.at(index).value.inventoryDefectId).subscribe( _=> {
+    //   this.dialog.open(MessageBoxComponent, { width: '350px', height: '100px', data: "Inventory Details deleted successfully !" });
+    //   setTimeout(() => {
+    //     this.defect.removeAt(index);
+    //     this.dialog.closeAll();
+    //   }, 5000);
+    // });
+  }
+
+  changeandunchangeForStudentLeave(flg:boolean , index : number){
+    // if(flg)
+    //   {
+    //     console.log(this.defect.at(index).value)
+    //    this.CreateInventoryDefect(this.defect.at(index).value);
+    //     this.defect.at(index).disable();
+    //   }
+    // else{
+    //     this.defect.at(index).enable();
+    // }
+  }
+
+  deleteForStaffLeave(index: number)
+  {
+    //console.log(this.studentLeave.at(index).value.inventoryDefectId);
+    // this.inventoryDefectService.deleteInventoryDefects(this.defect.at(index).value.inventoryDefectId).subscribe( _=> {
+    //   this.dialog.open(MessageBoxComponent, { width: '350px', height: '100px', data: "Inventory Details deleted successfully !" });
+    //   setTimeout(() => {
+    //     this.defect.removeAt(index);
+    //     this.dialog.closeAll();
+    //   }, 5000);
+    // });
+  }
+
+  changeandunchangeForStaffLeave(flg:boolean , index : number){
+    // if(flg)
+    //   {
+    //     console.log(this.defect.at(index).value)
+    //    this.CreateInventoryDefect(this.defect.at(index).value);
+    //     this.defect.at(index).disable();
+    //   }
+    // else{
+    //     this.defect.at(index).enable();
+    // }
   }
 
 }
---
+++ SchoolManage/SMS/SMS/SMS/Models/Leave/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMS.Models.Leave
+{
+	public class LeaveType
+	{
+		public int LeaveTypeId { get; set; }
+
+		public string LeaveTypeDescr { get; set; }
+
+		public int LeaveTypeFor { get; set; }
+	}
+}
